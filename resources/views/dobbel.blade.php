4ccc5b506ab456b9afa3997071733d7fa80ca67c
jxpsert <jplatenburg@outlook.com>
2024-11-16 01:29:59 +0100

    Add page when no trains; Fixes for page

--- resources/views/dobbel.blade.php
+++ resources/views/dobbel.blade.php
@@ -1,7 +1,11 @@
 @extends('layout')
 
 @section('content')
-    <h2 class="text-3xl font-bold text-blue-900">Je gaat naar...</h2>
+    @if ($departure !== null)
+        <h2 class="text-3xl font-bold text-blue-900">Je gaat naar...</h2>
+    @else
+        <h2 class="text-3xl font-bold text-blue-900">Jammer...</h2>
+    @endif
 
     <div class="mt-8">
         @if ($departure !== null)
@@ -10,25 +14,26 @@
                 <b>{{ $departure['departureDirection'] }}</b>
             </h2>
 
-            <div class="mt-8">
-                <h3 class="mt-4 text-2xl">die vertrekt vanaf <b>spoor
-                        {{ $departure['departureTrack'] }}</b> om
-                    <b>{{ \Carbon\Carbon::parse($departure['departureTime'])->setTimezone(config('app.timezone'))->format('H:i') }}
-                        uur</b>
-                </h3>
-            </div>
+            <h3 class="mt-2 text-2xl">die vertrekt vanaf <b>spoor
+                    {{ $departure['departureTrack'] }}</b> om
+                <b>{{ \Carbon\Carbon::parse($departure['departureTime'])->setTimezone(config('app.timezone'))->format('H:i') }}
+                    uur</b>
+            </h3>
 
             <a href="{{ route('dobbel', ['station' => $departure['stationUic']]) }}"
                 class="block p-4 mt-8 text-blue-900 bg-white rounded-lg">En nu?<br>Vind een trein vanuit
                 {{ $departure['stationName'] }}</a>
             <a href="{{ route('dobbel', ['station' => $departure['departureStation']]) }}"
                 class="block p-4 mt-2 text-white bg-blue-900 rounded-lg">Ik wil ergens anders heen</a>
+
+            <p class="mt-2 text-xl">Je vertrekt vanaf station <b>{{ $departure['departureStationName'] }}</b>.</p>
         @else
-            <h2 class="text-3xl">Helaas.</h2>
-            <p class="mt-2">Er is geen trein gevonden.</p>
+            <p class="mt-2 text-xl">Er komt binnenkort geen trein meer op dit station.
+                <br>Probeer het op een ander station, of kijk op de NS app of op <a href="https://www.ns.nl"
+                    class="text-blue-900 underline">ns.nl</a> voor actuele reisinformatie.
+            </p>
         @endif
 
-        <p class="mt-2 text-xl">Je vertrekt vanaf station <b>{{ $departure['departureStationName'] }}</b></p>
-        <a href="{{ route('home') }}" class="text-lg text-blue-900 underline">Beginnen op een ander station?</a>
+        <a href="{{ route('home') }}" class="mt-4 text-lg text-blue-900 underline">Beginnen op een ander station?</a>
     </div>
 @endsection
